a6023291c8e51fa68edce7774eb75050c4721732
Campol123 <66687172+Campol123@users.noreply.github.com>
2021-12-08 12:25:23 +0000

    Add files via upload

--- stockinfo.py
+++ stockinfo.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 
 def getStockInfo(stockSymbol):
-    infoParts = ["longName","zip","financialCurrency","currentPrice","grossProfits","country","enterpriseValue","website"]
+    infoParts = ["longName","zip","financialCurrency","currentPrice","regularMarketPrice","currency","grossProfits","country","enterpriseValue","website","CoinMarketCap"]
     stock = yf.Ticker(stockSymbol)
     info = stock.info
     curatedInfo = f"Info about Stock: {stockSymbol}:"
@@ -16,8 +16,12 @@ def getStockPrice(stockSymbol):
     stock = yf.Ticker(stockSymbol)
     info = stock.info
     infop = "currentPrice"
+    infop2 = "regularMarketPrice"
+    infop3 = "currency"
     if infop in info:
-        return f"Current price of {stockSymbol}: ${info[infop]}"
+        return f"Current price of {stockSymbol}: {info[infop]} {info[infop3].upper()}"
+    elif infop2 in info:
+        return f"Current price of {stockSymbol}: {info[infop2]} {info[infop3].upper()}"
     else:
         return f"Sorry, our API does not contain the current price of {stockSymbol}."
 
@@ -30,5 +34,8 @@ def checkStockValid(stockSymbol):
         return True
 
 
+#print(getStockPrice("BTC-GBP"))
+#print(getStockPrice("TSCO.L"))
 #print(getStockPrice("AMZN"))
-#print(getStockPrice("VOO"))
+#print(getStockPrice("vow3.De"))
+#print(getStockInfo("AMZN"))
