5b99b67fe5382f07cec96cd602f3542aa1a346fe
Campol123 <66687172+Campol123@users.noreply.github.com>
2021-12-11 00:52:38 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
-import discord
-import os
-from discord.ext import commands
-from stockinfo import getStockInfo,checkStockValid,getStockPrice
+import discord # imports discord module used to connect to the Discord API
+from discord.ext import commands # imports commands from Discord module
+from stockinfo import getStockInfo,checkStockValid,getStockPrice # imports custom functions from 'stockinfo.py'
 
-client = discord.Client()
-commands = ["/SG - brings up welcome text.","/SG help - brings up this menu.","/SG [Stock Symbol] info - returns info about given stock.","/SG [Stock Symbol] price - returns current price of given stock symbol."]
+client = discord.Client() # connnects to Discord
+
+botToken = "BotTokenGoesHere" # --Enter your bot token--
+
+commands = ["/SG - brings up welcome text.","/SG help - brings up this menu.","/SG [Stock Symbol] info - returns info about given stock.","/SG [Stock Symbol] price - returns current price of given stock symbol."] # array that hold the commands to be displayed in the help menu
+
 @client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+async def on_ready(): # code that runs once connected to Discord
+    print('We have logged in as {0.user}'.format(client)) # message to print once a connection has been made
+
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(message):# code that runs once a message is received
+
+    if message.author == client.user:# makes sure that the bot ignores it's own messages
         return
 
-    if message.content.startswith('/SG'):
-        helpText = ""
-        print(f"{message.author} ({message.author.id}) says \"{message.content}\"")
-        if message.content==("/SG"):
-            await message.channel.send(f'Hello! <@{message.author.id}>\nMy Name is Stock Genie!\nI am a Discord bot that can get you stock information.\ntry \"/SG help\" for available commands.')
+    if message.content.startswith('/SG'): #identifies the messages that start with '/SG' (Stock Genie)
+        helpText = "" #initialises the helpText variable that holds the help menu as a string
+        print(f"{message.author} ({message.author.id}) says \"{message.content}\"") # prints the message content and the user who posted it, aswell as their Discord ID
 
-        elif message.content==("/SG you smell"):
-            await message.channel.send(f'fuck right off <@{message.author.id}>')
+        if message.content==("/SG"): # identifies if they used the '/SG' command
+            await message.channel.send(f'Hello! <@{message.author.id}>\nMy Name is Stock Genie!\nI am a Discord bot that can get you stock information.\ntry \"/SG help\" for available commands.') # sends Discord message adressing the user who entered the command
 
-        elif message.content == ("/SG help"):
-            for x in range(len(commands)):
+        elif message.content == ("/SG help"): # identifies if the user used the '/SG help' command
+            for x in range(len(commands)): # constructs the help menu from the array
                 helpText += commands[x]
                 helpText += "\n"
+            await message.channel.send(f'{helpText}') # sends the help menu
 
-            await message.channel.send(f'{helpText}')
-
-
-        elif message.content[len(message.content)-4] == ("i") and message.content[len(message.content)-3] == ("n") and message.content[len(message.content)-2] == ("f") and message.content[len(message.content)-1] == ("o") and len(message.content)>9:
-
-            stockSymbol = message.content.replace("/SG ", "").replace(" info", "").upper()
-            if(checkStockValid(stockSymbol)):
-                info = getStockInfo(stockSymbol)
-                await message.channel.send(info)
+        elif message.content[len(message.content)-4] == ("i") and message.content[len(message.content)-3] == ("n") and message.content[len(message.content)-2] == ("f") and message.content[len(message.content)-1] == ("o") and len(message.content)>9: # identifies if the user used the '/SG <Stock Symbol> info' command
+            stockSymbol = message.content.replace("/SG ", "").replace(" info", "").upper() # finds the stock symbol from the command and makes it uppercase
+            if(checkStockValid(stockSymbol)): # checks the stock symbol is valid
+                info = getStockInfo(stockSymbol) # gets the stock symbol info
+                await message.channel.send(info) # sends the stock info
             else:
-                await message.channel.send(f'Invalid Stock Symbol\nTry: /SG help')
+                await message.channel.send(f'Invalid Stock Symbol\nTry: /SG help') # sends an error explaining it was an invalid stock symbol
 
-        elif message.content[len(message.content) - 5] == ("p") and message.content[len(message.content) - 4] == ("r") and message.content[len(message.content) - 3] == ("i") and message.content[len(message.content) - 2] == ("c") and message.content[len(message.content) - 1] == ("e") and len(message.content)>10:
-            stockSymbol = message.content.replace("/SG ", "").replace(" price", "").upper()
-            if (checkStockValid(stockSymbol)):
-                priceInfo = getStockPrice(stockSymbol)
-                await message.channel.send(priceInfo)
+        elif message.content[len(message.content) - 5] == ("p") and message.content[len(message.content) - 4] == ("r") and message.content[len(message.content) - 3] == ("i") and message.content[len(message.content) - 2] == ("c") and message.content[len(message.content) - 1] == ("e") and len(message.content)>10: # identifies if the user used the '/SG <Stock Symbol> price' command
+            stockSymbol = message.content.replace("/SG ", "").replace(" price", "").upper() # finds the stock symbol from the command and makes it uppercase
+            if (checkStockValid(stockSymbol)): # checks the stock symbol is valid
+                priceInfo = getStockPrice(stockSymbol) # gets the stock symbol price
+                await message.channel.send(priceInfo) # sends the stock symbol price
             else:
-                await message.channel.send(f'Invalid Stock Symbol\nTry: /SG help')
+                await message.channel.send(f'Invalid Stock Symbol\nTry: /SG help') # sends an error explaining it was an invalid stock symbol
 
         else:
-            await message.channel.send(f'Invalid Command')
+            await message.channel.send(f'Invalid Command') # sends an error explaining it was an invalid command
 
 
 
 
 
 
-client.run("OTE3NzY0NzMwNzI1MjIwMzUy.Ya9dAw.EKBAj6FnKPozbMtLRfF0TfcqB1Q")
+client.run(botToken) # connects to your Discord bot
--- stockinfo.py
+++ stockinfo.py
@@ -1,41 +1,37 @@
-import yfinance as yf
+import yfinance as yf # imports the stock information API
 
-def getStockInfo(stockSymbol):
-    infoParts = ["longName","zip","financialCurrency","currentPrice","regularMarketPrice","currency","grossProfits","country","enterpriseValue","website","CoinMarketCap"]
+def getStockInfo(stockSymbol): # defines the function to get stock info
+    infoParts = ["longName","zip","financialCurrency","currentPrice","regularMarketPrice","currency","grossProfits","country","enterpriseValue","website","CoinMarketCap"]  # an array that contains the keys of the stock symbol info dictionary returned by the API
     stock = yf.Ticker(stockSymbol)
-    info = stock.info
+    info = stock.info # gets stock info
     curatedInfo = f"Info about Stock: {stockSymbol}:"
     #print(info)
     for x in range(len(infoParts)):
         if infoParts[x] in info:
-            curatedInfo+="\n" + str(infoParts[x]) + ": " + str(info[infoParts[x]])
+            curatedInfo+="\n" + str(infoParts[x]) + ": " + str(info[infoParts[x]]) # assembles info text to be sent on Discord
 
     return curatedInfo
 
-def getStockPrice(stockSymbol):
+def getStockPrice(stockSymbol): # defines the function to get stock price
     stock = yf.Ticker(stockSymbol)
     info = stock.info
     infop = "currentPrice"
     infop2 = "regularMarketPrice"
     infop3 = "currency"
     if infop in info:
-        return f"Current price of {stockSymbol}: {info[infop]} {info[infop3].upper()}"
+        return f"Current price of {stockSymbol}: {info[infop]} {info[infop3].upper()}" # assembles price text to be sent on Discord
     elif infop2 in info:
-        return f"Current price of {stockSymbol}: {info[infop2]} {info[infop3].upper()}"
+        return f"Current price of {stockSymbol}: {info[infop2]} {info[infop3].upper()}" # assembles price text to be sent on Discord
     else:
-        return f"Sorry, our API does not contain the current price of {stockSymbol}."
+        return f"Sorry, our API does not contain the current price of {stockSymbol}." # returns an error stating the API does not have price information on a valid stock symbol
 
-def checkStockValid(stockSymbol):
+def checkStockValid(stockSymbol): # defines the function to check a stock symbol is valid
     stock = yf.Ticker(stockSymbol)
     info = stock.info
-    if(info["regularMarketPrice"]==None):
+    if(info["regularMarketPrice"]==None): # checks if stock symbol has a regular market price to determine it's validity
         return False
     else:
         return True
 
 
-#print(getStockPrice("BTC-GBP"))
-#print(getStockPrice("TSCO.L"))
-#print(getStockPrice("AMZN"))
-#print(getStockPrice("vow3.De"))
-#print(getStockInfo("AMZN"))
+
